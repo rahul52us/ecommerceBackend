2606004e65a2f25f45722370c61da95f715658f5
rahulkushwah <rahul.kushwah@sequelstring.com>
2025-10-24 15:29:16 +0530

    handle changes

--- src/routing/routingIndex.ts
+++ src/routing/routingIndex.ts
@@ -14,6 +14,7 @@ import dashboardRouting from "./dashboard/dashboard.routing";
 import eventRouting from './event/event.routing'
 import notificationRouting from './notification/notification.routing'
 import labRouting from "./lab/lab.routing";
+import masterRouting from "./masters/master.routing";
 
 const importRoutings = (app: any) => {
   app.use("/api/auth", userRouting);
@@ -32,6 +33,7 @@ const importRoutings = (app: any) => {
   app.use("/api/blog", blogRouting);
   app.use("/api/student", StudentRouting);
   app.use("/api/trip", TripRouting);
+  app.use('/api/masters',masterRouting)
 };
 
 export default importRoutings;
